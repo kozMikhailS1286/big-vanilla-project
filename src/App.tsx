7b0d93c224db10cabfe237dfa6c6e63a6dfc7388
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2023-02-28 16:50:32 +0300

    Vtornic Nedelya 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {getTriangleType} from "./lesson_8/lesson_8";
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
